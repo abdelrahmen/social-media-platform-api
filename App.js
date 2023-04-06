ff8e34250eb04a32de949493b584b927ce150cd9
Abdelrahmen <zzaazzaa1000@gmail.com>
2023-04-06 23:45:16 +0200

    implemented the login and delete user functions

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const path = require("path");
+
+const userRouter = require("./routers/user.router");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send(path.join('./public', 'index.html'));
+  res.send(path.join("./public", "index.html"));
 });
+
+app.use("/users", userRouter);
 
 const PORT = 3000 || process.env.PORT;
 app.listen(PORT, () => {
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -32,10 +32,48 @@ exports.registerUser = async (req, res) => {
   }
 };
 
+exports.loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+
+    res.status(200).json({ token });
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
 exports.getAllUsers = async (req, res, next) => {
   try {
     const users = await User.find({});
     res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+exports.deleteUser = async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json({ message: "User deleted successfully" });
   } catch (error) {
     res.status(500).json({ message: "Server error" });
   }
