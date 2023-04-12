833bc345c5dfd9aaf85908bf26c74fcc667ec071
Abdelrahmen <zzaazzaa1000@gmail.com>
2023-04-13 01:10:49 +0200

    exported the post model

--- __tests__/models/post.model.test.js
+++ __tests__/models/post.model.test.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
-const Post = require("../models/post.model.test");
+const Post = require("../../models/post.model");
+const User = require("../../models/user.model");
+
 require("dotenv").config();
 
 describe("Post model", () => {
@@ -8,10 +10,39 @@ describe("Post model", () => {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
+    require("dotenv").config();
   });
 
   afterAll(async () => {
     await mongoose.connection.close();
   });
 
+  it("has a required text field", async () => {
+    const post = new Post({
+      createdBy: new mongoose.Types.ObjectId(),
+    });
+    await expect(post.validate()).rejects.toThrow();
+  });
+
+  it("has an optional image field", async () => {
+    const post = new Post({
+      text: "Lorem ipsum",
+      createdBy: new mongoose.Types.ObjectId(),
+    });
+    await expect(post.validate()).resolves.not.toThrow();
+  });
+
+  it.skip("has a createdBy field that references a User", async () => {
+    const user = new User({ name: "Alice", email:"someemail@mail.com", password:'123' });
+    await user.save();
+
+    const post = new Post({
+      text: "Lorem ipsum",
+      createdBy: user._id,
+    });
+
+    await expect(post.validate()).resolves.not.toThrow();
+  });
+
+  
 });
