33a332934fc59ec685a89d94cd09df7f4057d7b9
Abdelrahmen <zzaazzaa1000@gmail.com>
2023-04-20 13:26:51 +0200

    added logging to the user controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,17 +1,22 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const User = require("../models/user.model");
+const Logger = require("../services/logger.service");
+
+const logger = new Logger("user.controller");
 
 exports.registerUser = async (req, res) => {
   const { username, email, password } = req.body;
 
   if (!username || !password || !email) {
+    logger.warn("Missing required fields in registerUser function");
     return res.status(400).json({ message: "All fields are required!" });
   }
 
   const user = await User.findOne({ email });
 
   if (user) {
+    logger.warn("User already registered in registerUser function");
     return res
       .status(400)
       .json({ message: "This user is already registered." });
@@ -26,8 +31,10 @@ exports.registerUser = async (req, res) => {
   });
 
   if (newUser) {
+    logger.info("New user registered in registerUser function");
     return res.status(201).json({ _id: newUser._id, email: User.email });
   } else {
+    logger.error("Error creating new user in registerUser function");
     return res.status(400).json({ message: "User data is not valid." });
   }
 };
@@ -39,12 +46,14 @@ exports.loginUser = async (req, res) => {
     const user = await User.findOne({ email });
 
     if (!user) {
+      logger.warn("Invalid email in loginUser function");
       return res.status(401).json({ message: "Invalid email" });
     }
 
     const isMatch = await bcrypt.compare(password, user.password);
 
     if (!isMatch) {
+      logger.warn("Incorrect password in loginUser function");
       return res.status(401).json({ message: "Incorrect password" });
     }
 
@@ -52,8 +61,10 @@ exports.loginUser = async (req, res) => {
       expiresIn: Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 30, //1 month
     });
 
+    logger.info("User logged in successfully in loginUser function");
     res.status(200).json({ token });
   } catch (error) {
+    logger.error(`Error in loginUser function: ${error.message}`);
     res.status(500).json({ message: "Server error" });
   }
 };
@@ -61,8 +72,10 @@ exports.loginUser = async (req, res) => {
 exports.getAllUsers = async (req, res, next) => {
   try {
     const users = await User.find({});
+    logger.info(`Retrieved ${users.length} users in getAllUsers function`);
     res.status(200).json(users);
   } catch (error) {
+    logger.error(`Error in getAllUsers function: ${error.message}`);
     res.status(500).json({ message: "Server error" });
   }
 };
@@ -71,10 +84,17 @@ exports.deleteUser = async (req, res) => {
   try {
     const user = await User.findByIdAndDelete(req.params.id);
     if (!user) {
+      logger.warn(
+        `User with ID ${req.params.id} not found in deleteUser function`
+      );
       return res.status(404).json({ message: "User not found" });
     }
+    logger.info(
+      `User with ID ${req.params.id} deleted successfully in deleteUser function`
+    );
     res.status(200).json({ message: "User deleted successfully" });
   } catch (error) {
+    logger.error(`Error in deleteUser function: ${error.message}`);
     res.status(500).json({ message: "Server error" });
   }
 };
